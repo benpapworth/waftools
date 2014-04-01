d194882b05c06a56d935f09df5ba38d2375bd3f0
michelm <michel.mooij7@gmail.com>
2014-04-01 16:10:24 +0200

    Added dist releases, added long_description

---
+++ README.txt
@@ -0,0 +1,6 @@
+Summary
+-------
+This package contains a collection of tools for the Waf_ build environment.
+
+.. _Waf: https://code.google.com/p/waf/
+
--- setup.py
+++ setup.py
@@ -7,26 +7,28 @@ from distutils.core import setup
 url = "https://bitbucket.org/Moo7/waftools"
 version = "0.0.9"
 
+with open('README.txt') as file:
+	long_description = file.read()
+
 setup(
-    name = "waftools",
-    packages = ["waftools"],
-    version = version,
-    description = "Handy tools for the WAF meta build environment",
-    author = "Michel Mooij",
-    author_email = "michel.mooij7@gmail.com",
-    url = url,
-    download_url = "%s/waftools-%s.zip" % (url, version),
-    keywords = ["waf", "cppcheck", "codeblocks", "eclipse", "make", "cmake", "c", "c++"],
-    classifiers = [
-        "Programming Language :: Python",
-        "Development Status :: 4 - Beta",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+	name = "waftools",
+	packages = ["waftools"],
+	version = version,
+	description = "Handy tools for the WAF meta build environment",
+	author = "Michel Mooij",
+	author_email = "michel.mooij7@gmail.com",
+	url = url,
+	download_url = "%s/waftools-%s.zip" % (url, version),
+	keywords = ["waf", "cppcheck", "codeblocks", "eclipse", "make", "cmake", "c", "c++"],
+	classifiers = [
+		"Programming Language :: Python",
+		"Development Status :: 4 - Beta",
+		"Environment :: Other Environment",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+		"Operating System :: OS Independent",
+		"Topic :: Software Development :: Libraries :: Python Modules",
         ],
-    long_description = """
-"""
+    long_description = long_description
 )
 
