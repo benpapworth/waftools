b5aa535e7288cfeabaa7a7dd982c4b7bb9091446
michelm <michel.mooij7@gmail.com>
2014-08-17 18:28:36 +0200

    issue #2,3 improved installation path of package data files.

--- setup.py
+++ setup.py
@@ -2,13 +2,24 @@
 # -*- encoding: utf-8 -*-
 # Michel Mooij, michel.mooij7@gmail.com
 
+import sys
+import site
 from distutils.core import setup
+import distutils.sysconfig
 import waftools
+
 
 url = "https://bitbucket.org/Moo7/waftools"
 
 with open('README.txt') as f:
 	long_description = f.read()
+
+
+if "--user" in sys.argv:
+	data_dir = '%s/waftools' % site.getusersitepackages()
+else:
+	data_dir = '%s/waftools' % distutils.sysconfig.get_python_lib()
+
 
 setup(
 	name = "waftools",
@@ -20,7 +31,7 @@ setup(
 	url = url,
 	download_url = "%s/waftools-%s.zip" % (url, waftools.version),
 	keywords = ["waf", "cppcheck", "codeblocks", "eclipse", "make", "cmake", "c", "c++", "msdev", "doxygen"],
-	data_files = [('Lib/site-packages/waftools', ['waftools/msdev.sln', 'waftools/doxy.config'])],
+	data_files = [(data_dir, ['waftools/msdev.sln', 'waftools/doxy.config'])],
 	classifiers = [
 		"Programming Language :: Python",
 		"Development Status :: 4 - Beta",
