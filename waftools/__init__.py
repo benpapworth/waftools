283e08f3ae63439de74153938302cc5884caea1a
michelm <michel.mooij7@gmail.com>
2014-08-22 12:31:37 +0200

    Moved 'options' and 'configure' to gnucc.

--- waftools/__init__.py
+++ waftools/__init__.py
@@ -8,39 +8,6 @@ version = "0.1.5"
 location = os.path.dirname(__file__)
 
 
-def options(opt):
-	'''Add default (C/C++) command line options.
-	
-	:param opt: options context 
-	:type opt: waflib.Options.OptionsContext
-	'''
-	opt.add_option('--check_c_compiler', dest='check_c_compiler', default='gcc', action='store', help='Selects C compiler type.')
-	opt.add_option('--check_cxx_compiler', dest='check_cxx_compiler', default='gxx', action='store', help='Selects C++ compiler type.')
-	opt.add_option('--debug', dest='debug', default=False, action='store_true', help='build with debug information.')
-
-
-def configure(conf):
-	'''Configures general environment settings; e.g. set default C/C++ compiler flags and defines 
-	based on the value of the command line --debug option.
-	
-	:param conf: configuration context 
-	:type conf: waflib.Configure.ConfigurationContext
-	
-	'''
-	if conf.options.debug:
-		flags = ['-Wall', '-g', '-ggdb']
-		defines = []
-	else:
-		flags = ['-Wall', '-O3']
-		defines = ['NDEBUG']
-
-	for cc in ('CFLAGS', 'CXXFLAGS'):
-		for flag in flags:
-			conf.env.append_unique(cc, flag)
-	for define in defines:
-		conf.env.append_unique('DEFINES', define)
-
-
 def get_scripts(top, name):
 	'''Returns a list of top level paths containing the specified file name.
 	
