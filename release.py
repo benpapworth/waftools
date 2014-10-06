aa2e190f4c72b1b341779367a0b5630020a81b7d
Michel <michel.mooij7@gmail.com>
2014-10-06 10:00:59 +0200

    prepareing for release...

--- release.py
+++ release.py
@@ -4,12 +4,14 @@
 import subprocess
 import waftools
 
-subprocess.call('waf distclean')
-subprocess.call('waf configure')
-subprocess.call('waf build')
+subprocess.call(['waf', 'distclean'])
+subprocess.call(['waf', 'configure'])
+subprocess.call(['waf', 'build'])
+subprocess.call(['waf', 'distclean'])
 
-subprocess.call('git tag -a v{0} -m "release {0}"'.format(waftools.version))
-subprocess.call('git push origin --tags')
+v = str(waftools.version)
+subprocess.call(['git', 'tag', '-a', 'v%s' % v, '-m', '"release %s"' % v])
+subprocess.call(['git', 'push', 'origin', '--tags'])
 
-subprocess.call('python setup.py sdist upload')
+subprocess.call(['python', 'setup.py', 'sdist', 'upload'])
 
